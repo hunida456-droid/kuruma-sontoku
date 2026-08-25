f6b10e7ebcea968fc16fe8f1dc4fc7eb1f9478c7
hunida456-droid <hunida456@gmail.com>
2026-08-25 22:25:34 +0900

    Connect Google Analytics

--- assets/site.js
+++ assets/site.js
@@ -1,1 +1,10 @@
 document.getElementById('year').textContent=new Date().getFullYear();
+
+const gaScript=document.createElement('script');
+gaScript.async=true;
+gaScript.src='https://www.googletagmanager.com/gtag/js?id=G-DDEYVWWEX7';
+document.head.appendChild(gaScript);
+window.dataLayer=window.dataLayer||[];
+function gtag(){dataLayer.push(arguments);}
+gtag('js',new Date());
+gtag('config','G-DDEYVWWEX7');
